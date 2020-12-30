dcaa52ae21ecfe31de6c942596c9108fbf16cbef
andrea <andrecasa91@gmail.com>
2020-12-30 22:01:00 +0100

    implementing c++ backend

--- JS-efg-addon/efgAddon.h
+++ JS-efg-addon/efgAddon.h
@@ -1,48 +1,45 @@
 // https://napi.inspiredware.com/getting-started/objectwrap.html#src-object-wrap-demo-cc-and-src-object-wrap-demo-h
 
 #include <napi.h>
-#include <Parser.h>
-#include <functional>
-#include "src/OptionalString.h"
-#include "src/JsonArrayStream.h"
+#include <Graph.h>
+#include <set>
 
-class xmlJS : public Napi::ObjectWrap<xmlJS> {
+class efgJS : public Napi::ObjectWrap<efgJS> {
 public:
     static Napi::Object Init(Napi::Env env, Napi::Object exports);
-    xmlJS(const Napi::CallbackInfo& info);
+    efgJS(const Napi::CallbackInfo& info);
 
     Napi::Value ProcessRequest(const Napi::CallbackInfo&);
 
 private:
-    enum nodeType{ undefined, tag, attribute};
-    nodeType GetNodeType(const std::size_t& id);
+    std::string getJSON();
 
     void Import(const std::string& fileName);
 
+    void Append(const std::string& fileName);
+
     void Export(const std::string& fileName);
 
-    void Delete(const std::size_t& id);
+    void CreateIsolatedVar(const std::string& name, const std::size_t& size);
 
-    void Rename(const std::size_t& id, const std::string& newName);
+    void AddObservation(const std::vector<std::pair<std::string, std::size_t>>& obs);
 
-    void NestTag(const std::size_t& parentId, const std::string& tagName);
+    void DeleteObservation();
 
-    void NestAttribute(const std::size_t& parentId, const std::string& attrName);
+    std::vector<float> GetMarginals(const std::string& name);
 
-    void SetValue(const std::size_t& id, const std::string& value);
+    std::vector<std::size_t> GetMap();
+
+    void AddFactor(const std::string& name, const std::string& fileName, const float& weight = 0.f);
+
+    void AddFactor(const std::string& nameA, const std::string& nameB, const std::string& fileName, const float& weight = 0.f);
+
+    void AddFactor(const std::string& nameA, const std::string& nameB, const bool& corr_anti, const float& weight = 0.f);
 
 private:
-    struct xmlNodePosition {
-        std::vector<std::string> pathFromRoot;
-        OptionalString           attributeName; // empty for tag
-    };    
-
-    void updateJsonNodes();
-    void updateJsonTag(JSONArrayStream& nodes, JSONArrayStream& edges, std::size_t& counter, const xmlPrs::Tag& tag, const xmlNodePosition& parentPosition, const std::size_t& parentId);
-
     std::map<std::string, std::function<std::string(const Napi::CallbackInfo&)>> commands;
+
 // data
-    xmlPrs::Parser                         data;
-    std::map<std::size_t, xmlNodePosition> nodesInfo;
-    std::string                            dataJSON;
+    std::unique_ptr<EFG::model::Graph> graph;
+    std::set<EFG::CategoricVariable> isolatedVars;
 };
--- JS-efg-addon/src/efgAddon.cc
+++ JS-efg-addon/src/efgAddon.cc
@@ -1,10 +1,6 @@
-#include "../xmlAddon.h"
+#include "../efgAddon.h"
 #include <iostream>
 using namespace Napi;
-
-#define COLOR_TAG "#0A5407"
-#define COLOR_TAG_ROOT "#D26C00"
-#define COLOR_ATTR "#A59407"
 
 #define ARGS_CHECK(NUMBERS_EXPECTED) \
   Napi::Env env = info.Env(); \
@@ -21,9 +17,11 @@ using namespace Napi;
 
 #define AS_STRING(POSITION) std::string(info[POSITION].As<Napi::String>().Utf8Value())
 
-#define AS_SIZE_T(POSITION) static_cast<std::size_t>(std::atoi(AS_STRING(POSITION).c_str()))
+bool operator<(const EFG::CategoricVariable& a, const EFG::CategoricVariable& b) {
+  return (a.GetName() < b.GetName());
+}
 
-xmlJS::xmlJS(const Napi::CallbackInfo& info) 
+efgJS::efgJS(const Napi::CallbackInfo& info) 
   : ObjectWrap(info) {
   ARGS_CHECK(0)
   this->updateJsonNodes();
@@ -88,7 +86,7 @@ xmlJS::xmlJS(const Napi::CallbackInfo& info)
   });
 }
 
-Napi::Value xmlJS::ProcessRequest(const Napi::CallbackInfo& info){
+Napi::Value efgJS::ProcessRequest(const Napi::CallbackInfo& info){
   Napi::Env env = info.Env(); 
     if (info.Length() == 0) { 
       Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
@@ -104,236 +102,116 @@ Napi::Value xmlJS::ProcessRequest(const Napi::CallbackInfo& info){
   return Napi::String::New(env, it->second(info).c_str());
 }
 
-void printTag(JSONArrayStream& nodes, const std::size_t& counter, const std::string& name, const bool& isRoot = false){
-  std::stringstream s;
-  s << "{\"label\":\"<" << name << ">\"";
-  s << ",\"color\":\"";
-  if(isRoot){
-    s << COLOR_TAG_ROOT;
-  }
-  else{
-    s << COLOR_TAG;
-  }
-  s << "\"";
-  s << ",\"id\":\"" << counter << "\"";
-  s << "}";
-  nodes << s.str();
-};
 
-void printAttribute(JSONArrayStream& nodes, const std::size_t& counter, const std::string& name, const std::string& value){
-  std::stringstream s;
-  s << "{\"label\":\"" << name << "=" << value << "\"";
-  s << ",\"color\":\"" << COLOR_ATTR << "\"";
-  s << ",\"id\":\"" << counter << "\"";
-  s << ",\"shape\":\"box\"";
-  s << "}";
-  nodes << s.str();
-};
 
-void printEdge(JSONArrayStream& edges, const std::size_t& from, const std::size_t& to, const bool& leads2Tag){
-  std::stringstream s;
-  s << "{\"from\":\"" << from << "\"";
-  s << ",\"to\":\"" << to << "\"";
-  if(leads2Tag){
-    s << ",\"color\":\"" << COLOR_TAG << "\"";
-    s << ",\"arrows\":\"from\"";
-  }
-  else{
-    s << ",\"color\":\"" << COLOR_ATTR << "\"";
-  }
-  s << "}";
-  edges << s.str();
-};
 
-void xmlJS::updateJsonTag(JSONArrayStream& nodes, JSONArrayStream& edges, std::size_t& counter, const xmlPrs::Tag& tag, const xmlNodePosition& parentPosition, const std::size_t& parentId) {
-  xmlNodePosition position(parentPosition);
-  position.pathFromRoot.push_back(tag.getName());
-  ++counter;
-  this->nodesInfo.emplace(counter, position);
-  printTag(nodes, counter, tag.getName());
-  printEdge(edges, parentId, counter, true);
-  std::size_t thisId = counter;
-  const std::multimap<std::string, std::string>& attributes = tag.getAttributesConst();
-  for(auto it= attributes.begin(); it!=attributes.end(); ++it){
-    xmlNodePosition attrPos(position);
-    attrPos.attributeName.set(it->first);
-    ++counter;
-    this->nodesInfo.emplace(counter, attrPos);
-    printAttribute(nodes, counter, it->first, it->second);   
-    printEdge(edges, thisId, counter, false);   
-  }
-  xmlPrs::Tag::ConstIterator nestedTags = tag.getNestedAllConst();
-  for(auto it= nestedTags.begin(); it!=nestedTags.end(); ++it){
-    this->updateJsonTag( nodes, edges, counter, *it->second, position, thisId);
-  }
-}
 
-void xmlJS::updateJsonNodes() {
-  this->nodesInfo.clear();
-  std::size_t counter = 0;
-  xmlNodePosition rootPosition;
-  this->nodesInfo.emplace(counter, rootPosition);
-  JSONArrayStream nodes, edges;
-  printTag(nodes, 0, this->data.getRoot().getName(), true);
-  const std::multimap<std::string, std::string>& attributes = this->data.getRoot().getAttributesConst();
-  for(auto it= attributes.begin(); it!=attributes.end(); ++it){
-    xmlNodePosition attrPos(rootPosition);
-    attrPos.attributeName.set(it->first);
-    ++counter;
-    this->nodesInfo.emplace(counter, attrPos);
-    printAttribute(nodes, counter, it->first, it->second);   
-    printEdge(edges, 0, counter, false);   
-  }
-  xmlPrs::Tag::ConstIterator rootNested = this->data.getRoot().getNestedAllConst();
-  for(auto it = rootNested.begin(); it!=rootNested.end(); ++it){
-    this->updateJsonTag(nodes, edges, counter, *it->second, rootPosition, 0);
-  }
-
-  std::stringstream json;
-  json << "{\"nodes\":";
-  json << nodes.reset();
-  json << ",\"edges\":";
-  json << edges.reset();
-  json << "}";
-  this->dataJSON = json.str();
-}
-
-xmlJS::nodeType xmlJS::GetNodeType(const std::size_t& id) {
-  nodeType result = nodeType::undefined;
-  auto it = this->nodesInfo.find(id);
-  if(it!=this->nodesInfo.end()) {
-    if(nullptr == it->second.attributeName) {
-      result = nodeType::tag;
-      std::cout << "is a tag" << std::endl;
-    }
-    else{
-      result = nodeType::attribute;
-      std::cout << "is an attribute" << std::endl;
-    }
-  }
-  else {
-    std::cout << "undefined node type" << std::endl;
-  }
-  return result;
-}
-
-void xmlJS::Import(const std::string& fileName){
+void efgJS::Import(const std::string& fileName) {
+  std::unique_ptr<EFG::model::Graph> model;
   try {
-    std::cout << "importing " << fileName << std::endl;
-    xmlPrs::Parser newData(fileName);
-    std::cout << "success" << std::endl;
-    this->data = std::move(newData);
-    this->updateJsonNodes();
+    model = std::make_unique<EFG::model::Graph>(fileName);
   }
   catch(...) {
-    return;
+    model.reset();
+  }
+  if(nullptr != model) {
+    this->graph = std::move(model);
+    this->isolatedVars.clear();
   }
 }
 
-void xmlJS::Export(const std::string& fileName){
+void efgJS::Append(const std::string& fileName) {
+  if(nullptr == this->graph){
+    this->Import(fileName);
+    return;
+  }
+
+  std::unique_ptr<EFG::model::Graph> model;
   try {
-    std::cout << "exporting " << fileName << std::endl;
-    this->data.reprint(fileName);
-    std::cout << "success" << std::endl;
+    model = std::make_unique<EFG::model::Graph>(fileName);
   }
   catch(...) {
-    return;
+    model.reset();
+  }
+  if(nullptr != model) {
+    this->graph->Insert(model->GetStructure());
+    this->isolatedVars.clear();
   }
 }
 
-void xmlJS::Delete(const std::size_t& id){
-  if(id == 0) {
-    std::cout << "request to delete root refused" << std::endl;
-    return;
+void efgJS::Export(const std::string& fileName){
+  if(nullptr == this->graph) return;
+  this->graph->Reprint(fileName);
+}
+
+void efgJS::CreateIsolatedVar(const std::string& name, const std::size_t& size){
+  if((nullptr != this->graph) && (nullptr != this->graph->FindVariable(name))) return;
+  EFG::CategoricVariable var(size, name);
+  if(this->isolatedVars.find(var) != this->isolatedVars.end()) return;
+  this->isolatedVars.emplace(var);
+}
+
+void efgJS::AddObservation(const std::vector<std::pair<std::string, std::size_t>>& obs) {
+  if(nullptr == this->graph) return;
+  auto obOld = this->graph->GetObservationSet();
+  std::vector<std::pair<std::string, std::size_t>> ob;
+  ob.reserve(obOld.size() + obs.size());
+  for(auto it = obOld.begin(); it!=obOld.end(); ++it){
+    ob.push_back(std::make_pair(it->first->GetName(), it->second));
   }
-  auto it = this->nodesInfo.find(id);
-  if(it != this->nodesInfo.end()){
-    xmlPrs::Tag& tag = this->data.getRoot().getNested(it->second.pathFromRoot);
-    if(nullptr == it->second.attributeName) {
-      std::cout << "deleting tag " << tag.getName() << std::endl;
-      tag.remove();
+  for(auto it = obs.begin(); it!=obs.end(); ++it) {
+    auto* var = this->graph->FindVariable(it->first);
+    if((nullptr != var) && (it->second < var->size())){
+      ob.push_back(std::make_pair(it->first, it->second));
     }
-    else{
-      std::cout << "deleting attribute " << *it->second.attributeName << std::endl;
-      auto itA = this->data.getRoot().getAttributes().find( *it->second.attributeName );
-      this->data.getRoot().getAttributes().erase(itA);
-    }
-    this->updateJsonNodes();
-    return;
   }
-  std::cout << "delete undefined node" << std::endl;
+  this->graph->SetEvidences(ob);
 }
 
-void xmlJS::Rename(const std::size_t& id, const std::string& newName){
-  auto it = this->nodesInfo.find(id);
-  if(it != this->nodesInfo.end()){
-    xmlPrs::Tag& tag = this->data.getRoot().getNested(it->second.pathFromRoot);
-    if(nullptr == it->second.attributeName) {
-      std::cout << "renaming tag " << tag.getName() << std::endl;
-      tag.setName(newName);
-    }
-    else{
-      std::cout << "renaming attribute " << *it->second.attributeName << std::endl;
-      tag.setAttributeName(*it->second.attributeName, newName);
-    }
-    this->updateJsonNodes();
-    return;
-  }
-  std::cout << "rename undefined node" << std::endl;
+void efgJS::DeleteObservation(){
+  if(nullptr == this->graph) return;
+  this->graph->SetEvidences(std::vector<std::pair<std::string, std::size_t>>{});
 }
 
-void xmlJS::NestTag(const std::size_t& parentId, const std::string& tagName){
-  auto it = this->nodesInfo.find(parentId);
-  if(it != this->nodesInfo.end() && (nullptr == it->second.attributeName)){
-    xmlPrs::Tag& tag = this->data.getRoot().getNested(it->second.pathFromRoot);
-    std::cout << "nest tag " << tagName << " to tag " << tag.getName() << std::endl;
-    tag.addNested(tagName);
-    this->updateJsonNodes();
-    return;
+std::vector<float> efgJS::GetMarginals(const std::string& name) {
+  if(nullptr == this->graph) return {};
+  std::vector<float> marginals;
+  try {
+    marginals = this->graph->GetMarginalDistribution(name);
   }
-  std::cout << "nest tag to undefined node" << std::endl;
+  catch(...) {
+    return  {};
+  }
+  return marginals;
 }
 
-void xmlJS::NestAttribute(const std::size_t& parentId, const std::string& attrName){
-  auto it = this->nodesInfo.find(parentId);
-  if(it != this->nodesInfo.end() && (nullptr == it->second.attributeName)){
-    xmlPrs::Tag& tag = this->data.getRoot().getNested(it->second.pathFromRoot);
-    std::cout << "nest attribute " << attrName << " to tag " << tag.getName() << std::endl;
-    tag.getAttributes().emplace(attrName, "undefined");
-    this->updateJsonNodes();
-    return;
+std::vector<std::size_t> efgJS::GetMap() {
+  if(nullptr == this->graph) return {};
+  std::vector<std::size_t> map;
+  try {
+    map = this->graph->GetMAP();
   }
-  std::cout << "nest attribute to undefined node" << std::endl;
+  catch(...) {
+    return  {};
+  }
+  return map;
 }
 
-void xmlJS::SetValue(const std::size_t& id, const std::string& value){
-  auto it = this->nodesInfo.find(id);
-  if(it != this->nodesInfo.end() && (nullptr != it->second.attributeName)){
-    xmlPrs::Tag& tag = this->data.getRoot().getNested(it->second.pathFromRoot);
-    std::cout << "set value of attribute in tag " << tag.getName() << std::endl;
-    auto r = tag.getAttributes().equal_range(*it->second.attributeName);
-    r.first->second = value;
-    this->updateJsonNodes();
-    return;
-  }
-  std::cout << "set value of undefined node" << std::endl;
-}
-
-Napi::Object xmlJS::Init(Napi::Env env, Napi::Object exports) {
+Napi::Object efgJS::Init(Napi::Env env, Napi::Object exports) {
   Napi::HandleScope scope(env);
 
-  Napi::Function func = DefineClass(env, "xmlJS", {
-    InstanceMethod("ProcessRequest", &xmlJS::ProcessRequest)
+  Napi::Function func = DefineClass(env, "efgJS", {
+    InstanceMethod("ProcessRequest", &efgJS::ProcessRequest)
   });
 
   Napi::FunctionReference constructor;
   constructor = Napi::Persistent(func);
-  exports.Set("xmlJS", func);
+  exports.Set("efgJS", func);
   return exports;
 }
 
 Napi::Object Init (Napi::Env env, Napi::Object exports) {
-    xmlJS::Init(env, exports);
+    efgJS::Init(env, exports);
     return exports;
 }
 
