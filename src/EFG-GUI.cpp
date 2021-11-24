fa5b13db8319825049db09c14c41444ae226e1f8
Andrea Casalino <andrea.casalino@cloudnc.com>
2021-11-24 17:24:55 +0000

    fixing default example suggestion

--- src/EFG-GUI.cpp
+++ src/EFG-GUI.cpp
@@ -1,27 +1,34 @@
+#include <GraphShell.h>
 #include <httplib.h>
 #include <iostream>
-#include <GraphShell.h>
 
 using namespace httplib;
 using namespace std;
 
-std::string getRequestName(const std::string& incompleteName) {
+std::string getRequestName(const std::string &incompleteName) {
   std::string temp = "/" + incompleteName;
   return temp;
 }
 
-#define REACTION(SYMBOL, METHOD) \
-  svr.Post(getRequestName(SYMBOL).c_str() , [&model, &nullResp](const Request& req, Response& res) { \
-    std::cout << "request ID: " << SYMBOL << std::endl; \
-    std::cout << "request:" << std::endl << req.body << std::endl; \
-    res.set_header("Access-Control-Allow-Origin", "*"); \
-    gui::RequestPtr opt = gui::RequestOptions::parse(req.body); \
-    if(nullptr == opt) { \
-      res.set_content(nullResp.str(), "text/plain"); \
-    } \
-    res.set_content(model.METHOD(*opt), "text/plain"); \
-    std::cout << "response:" << std::endl << res.body << std::endl << std::endl; \
-  });
+#define REACTION(SYMBOL, METHOD)                                               \
+  svr.Post(getRequestName(SYMBOL).c_str(),                                     \
+           [&model, &nullResp](const Request &req, Response &res) {            \
+             std::cout << "request ID: " << SYMBOL << std::endl;               \
+             std::cout << "request:" << std::endl << req.body << std::endl;    \
+             res.set_header("Access-Control-Allow-Origin", "*");               \
+             gui::RequestPtr opt = gui::RequestOptions::parse(req.body);       \
+             if (nullptr == opt) {                                             \
+               res.set_content(nullResp.str(), "text/plain");                  \
+             }                                                                 \
+             res.set_content(METHOD(*opt), "text/plain");                      \
+             std::cout << "response:" << std::endl                             \
+                       << res.body << std::endl                                \
+                       << std::endl;                                           \
+           });
+
+static const std::string default_example =
+    std::string(EXAMPLE_FOLDER) +
+    std::string("Sample06-Learning-A/graph_3.xml");
 
 int main() {
 
@@ -30,29 +37,41 @@ int main() {
 
   const GraphJSON nullResp;
 
-  REACTION("X", Import)
+  svr.Post(getRequestName("D").c_str(), [&model, &nullResp](const Request &req,
+                                                            Response &res) {
+    std::cout << "request ID: "
+              << "D" << std::endl;
+    res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_content(default_example, "text/plain");
+    std::cout << "response:" << std::endl << res.body << std::endl << std::endl;
+  });
 
-  REACTION("A", Append)
+  REACTION("X", model.Import)
 
-  REACTION("V", CreateIsolatedVar)
+  REACTION("A", model.Append)
 
-  REACTION("M", RecomputeMap)
+  REACTION("V", model.CreateIsolatedVar)
 
-  REACTION("O", ResetObservations)
+  REACTION("M", model.RecomputeMap)
 
-  REACTION("P", AddFactor)
+  REACTION("O", model.ResetObservations)
 
-  REACTION("R", Export)
+  REACTION("P", model.AddFactor)
 
-  REACTION("Q", GetVariableInfo)
+  REACTION("R", model.Export)
 
-  REACTION("I", GetMarginals)
+  REACTION("Q", model.GetVariableInfo)
 
-  svr.Post(getRequestName("S").c_str(), [&model, &nullResp](const Request& req, Response& res) { \
-    std::cout << "request ID: " << "S" << std::endl;
+  REACTION("I", model.GetMarginals)
+
+  svr.Post(getRequestName("S").c_str(), [&model, &nullResp](const Request &req,
+                                                            Response &res) {
+    std::cout << "request ID: "
+              << "S" << std::endl;
     res.set_header("Access-Control-Allow-Origin", "*");
     GraphJSON resp;
-    std::unique_ptr<gui::json::structJSON> folder = std::make_unique<gui::json::structJSON>();
+    std::unique_ptr<gui::json::structJSON> folder =
+        std::make_unique<gui::json::structJSON>();
     folder->addElement("folder", gui::json::String(SOURCE_FOLDER));
     resp.setInfo(std::move(folder));
     res.set_content(resp.str(), "text/plain");
